f7ccdaa9023ad9c94c755ad4ed0bc923685fbef1
Taha Pourjalali <taha.hojati@gmail.com>
2016-02-14 00:37:08 -1000

    now the search function fires everytime i type something.  still buggy though..

--- src/js/all.js
+++ src/js/all.js
@@ -11,14 +11,39 @@ function initMap() {
 }
 
 function AppViewModel() {
-	this.allshops = ko.observableArray([
+	this.searchTerm = ko.observable("");
+	this.allshops = [
 		new CoffeePlace("The Curb Kaimuki", "3538 Waialae Ave\nHonolulu, HI 96816", "hello"),
 		new CoffeePlace("Stir", "3583 Waialae Ave\nHonolulu, HI 96816", "dfa"),
 		new CoffeePlace("Island Vintage Coffee", "2301 Kalakaua Ave\nHonolulu, HI 96815", "dfa"),
 		new CoffeePlace("Kai Coffee", "2424 Kalakaua Ave\nHonolulu, HI 96815", "dfa"),
 		new CoffeePlace("Brue Bar", "119 Merchant St\nHonolulu, HI 96813", "dfa"),
 		new CoffeePlace("Insomnia", "669 Auahi St\nHonolulu, HI 96813", "dfa")
-	]);
+	];
+	this.shops = ko.observableArray(this.allshops);
+	this.dosearch = function() {
+		console.log("called");
+		console.log("hello" + this.searchTerm());
+
+		if (this.searchTerm() === "" ) {
+
+			return this.allshops;
+		} else {
+			var r = [];
+			var len = this.allshops().length;
+			for (var i = 0; i < len; ++i) {
+				console.log(this.allshops()[i].name);
+				if (this.allshops()[i].name.search(this.searchTerm()) != -1)
+					r.push(this.allshops()[i]);
+			}
+		}
+	};
+	this.dosearch2= function(){
+		document.getElementById('placesDiv').innerHTML='hello world'
+	}
+		//console.log(this.dosearch2);
+		this.searchTerm.subscribe(this.dosearch);
+
 }
 
 function CoffeePlace(name, address, yelpURL) {
@@ -28,4 +53,5 @@ function CoffeePlace(name, address, yelpURL) {
 }
 
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
+var main = new AppViewModel();
+ko.applyBindings(main);
