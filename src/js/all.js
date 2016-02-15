96768e6d018a17f8a57348149569fa8681c01511
Taha Pourjalali <taha.hojati@gmail.com>
2016-02-14 20:40:33 -1000

    clicking on markers causes Hello. TODO: marker animation

--- src/js/all.js
+++ src/js/all.js
@@ -1,12 +1,12 @@
 function AppViewModel() {
 	this.searchTerm = ko.observable("");
 	this.allshops = [
-		new CoffeePlace("The Curb Kaimuki", "3538 Waialae Ave\nHonolulu, HI 96816", "hello", 21.2834,-157.80026),
-		new CoffeePlace("Stir", "3583 Waialae Ave\nHonolulu, HI 96816", "dfa", 21.294237, -157.812856),
-		new CoffeePlace("Island Vintage Coffee", "2301 Kalakaua Ave\nHonolulu, HI 96815", "dfa",21.278211, -157.828591),
-		new CoffeePlace("Kai Coffee", "2424 Kalakaua Ave\nHonolulu, HI 96815", "dfa",21.275869, -157.824931),
-		new CoffeePlace("Brue Bar", "119 Merchant St\nHonolulu, HI 96813", "dfa",21.308513, -157.862892),
-		new CoffeePlace("Insomnia", "669 Auahi St\nHonolulu, HI 96813", "dfa",21.298579, -157.861207)
+		new CoffeePlace("The Curb Kaimuki", "3538 Waialae Ave\nHonolulu, HI 96816", "the-curb-kaimuki-honolulu-2", 21.2834, -157.80026),
+		new CoffeePlace("Stir", "3583 Waialae Ave\nHonolulu, HI 96816", "stir-honolulu", 21.294237, -157.812856),
+		new CoffeePlace("Island Vintage Coffee", "2301 Kalakaua Ave\nHonolulu, HI 96815", "island-vintage-coffee-honolulu-4", 21.278211, -157.828591),
+		new CoffeePlace("Kai Coffee", "2424 Kalakaua Ave\nHonolulu, HI 96815", "kai-coffee-hawaii-honolulu", 21.275869, -157.824931),
+		new CoffeePlace("Brue Bar", "119 Merchant St\nHonolulu, HI 96813", "brue-bar-honolulu", 21.308513, -157.862892),
+		new CoffeePlace("Insomnia", "669 Auahi St\nHonolulu, HI 96813", "insomnia-honolulu", 21.298579, -157.861207)
 	];
 	this.shops = ko.observableArray(this.allshops.slice());
 	this.dosearch = function() {
@@ -17,13 +17,12 @@ function AppViewModel() {
 			mythis.shops.removeAll();
 			var len = mythis.allshops.length;
 			for (var i = 0; i < len; ++i) {
-				if (mythis.allshops[i].name.search(new RegExp(mythis.searchTerm(),"i")) != -1) {
+				if (mythis.allshops[i].name.search(new RegExp(mythis.searchTerm(), "i")) != -1) {
 					console.log(mythis.allshops[i].name);
 					mythis.shops.push(mythis.allshops[i]);
-					markvisible(true,i);
-				}
-				else
-					markvisible(false,i);
+					markvisible(true, i);
+				} else
+					markvisible(false, i);
 			}
 		};
 	};
@@ -35,14 +34,21 @@ function AppViewModel() {
 
 }
 
-function CoffeePlace(name, address, yelpURL, mylat, mylon) {
+function CoffeePlace(name, address, yelpID, mylat, mylon) {
 	this.address = address;
 	this.name = name;
-	this.yelpURL = yelpURL;
+	this.yelpID = yelpID;
 	this.latlng = {
 		lat: mylat,
 		lng: mylon
 	};
+	this.clousure = function() {
+		var mythis = this;
+		return function(data) {
+			console.log(data);
+		}
+	};
+	$.getJSON('https://api.yelp.com/v2/business/' + this.yelpID,{Consumer} ,this.clousure());
 }
 
 // Activates knockout.js
@@ -53,7 +59,35 @@ ko.applyBindings(main);
 
 //GoogleMaps
 var map;
-var markers; 
+var markers;
+
+function MarkerAndInfo(latlng, mymap, mytitle, infoString) {
+	this.googleMarker = new google.maps.Marker({
+		position: new google.maps.LatLng(latlng),
+		map: mymap,
+		title: mytitle
+	});
+	this.infoWindow = new google.maps.InfoWindow({
+		content: infoString,
+		maxWidth: 200
+	});
+	this.clousure = function() {
+		var mythis = this;
+		return function() {
+			console.log(mythis.infoWindow);
+			mythis.infoWindow.open(map, mythis.googleMarker);
+		}
+	};
+	this.clousure = function() {
+		var mythis = this;
+		return function() {
+			console.log(mythis.infoWindow);
+			mythis.infoWindow.open(map, mythis.googleMarker);
+		};
+	};
+	this.googleMarker.addListener('click', this.clousure());
+}
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {
@@ -64,14 +98,11 @@ function initMap() {
 	});
 	markers = [];
 	for (var i = 0; i < main.allshops.length; ++i) {
-		markers.push(new google.maps.Marker({
-			position: new google.maps.LatLng(main.allshops[i].latlng),
-			map: map,
-			title: main.allshops[i].name
-		}));
+		markers.push(new MarkerAndInfo(main.allshops[i].latlng, map, main.allshops[i].name, "Hello"));
 	}
 
 }
-function markvisible(value, index){
-	markers[index].setVisible(value);
+
+function markvisible(value, index) {
+	markers[index].googleMarker.setVisible(value);
 }
